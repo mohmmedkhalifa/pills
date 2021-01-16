95ee5d5444dcde9a1370f6cfe893b9a94c619676
Mohammed <asdasdm94@gmail.com>
2021-01-16 21:16:47 +0200

    for Nourhan

---
+++ lib/models/drug_model.dart
@@ -0,0 +1,40 @@
+import 'package:pills_reminder/services/db_helper.dart';
+
+class DrugModel {
+  int id;
+  String drugname;
+  String drugdose;
+  // int date;
+  // String time;
+
+  bool isMedicine;
+
+  DrugModel({
+    this.id,
+    this.drugname,
+    this.drugdose,
+    // this.time,
+    // this.date,
+    this.isMedicine,
+  });
+
+  DrugModel.fromMap(Map map) {
+    this.id = map[DBHelper.drugIdColumn];
+    this.drugname = map[DBHelper.drugNameColumnn];
+    this.drugdose = map[DBHelper.drugDoseColumn];
+    // this.date = map[DBHelper.drugDateColumnn];
+    // this.time = map[DBHelper.drugTimeColumn];
+    this.isMedicine =
+        map[DBHelper.drugIsMedicineColumnName] == 1 ? true : false;
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      DBHelper.drugNameColumnn: this.drugname,
+      DBHelper.drugDoseColumn: this.drugdose,
+      // DBHelper.drugDateColumnn: this.date,
+      // DBHelper.drugTimeColumn: this.time,
+      DBHelper.drugIsMedicineColumnName: this.isMedicine ? 1 : 0
+    };
+  }
+}
